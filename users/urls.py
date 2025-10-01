ec13a75e32b429dcea5e3ab9125f550891e9f624
fayzulloxo8 <solijonovfayzullox@gmail.com>
2025-10-01 20:34:17 +0500

    .

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterView, UserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, UserView, admin_stats  # ✅ admin_statsni shu yerda import qilamiz
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", UserView.as_view(), name="user-detail"),
+    path("admin-stats/", admin_stats, name="admin-stats"),  # ✅ admin stats endpoint
 ]
